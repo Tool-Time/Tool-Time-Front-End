9ea38324b943df6bbbfff8c6db6904736b8af40c
CullenSharp2 <cullen.sharp100@gmail.com>
2021-05-04 10:27:26 -0700

    Create static header

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,6 +1,13 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
 function Footer() {
     return (
-        <h1>Tool-time</h1>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>Tool Time</Navbar.Brand>
+        <Button variant="outline-success" className="mr-2">Login</Button>
+        <Button variant="outline-success">Logout</Button>
+      </Navbar>
     )
 }
 
