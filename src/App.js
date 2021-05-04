bd91f08167582680d53959c6c5dfa93e9c7cf2f3
CullenSharp2 <cullen.sharp100@gmail.com>
2021-05-04 12:02:37 -0700

    Implement react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import React from 'react';
 import { Container, Card, Form, Button, Navbar} from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Header from './modules/Header';
 
 
@@ -14,23 +19,31 @@ class App extends React.Component {
 
   render(){
     return(
-      <Container fluid>
-        <Header />
-        <Navbar bg="success">
-          <Form inline>
-            <Form.Group >
-              <Form.Control type="text" placeholder="enter a category of tool">
-              </Form.Control>
-              <Button className="ml-2">Submit</Button>
-            </Form.Group>
-          </Form>
-        </Navbar>
-          <Card.Img
-          alt='kitten' 
-          src='http://placekitten.com/1920/600'
-          className="radius-0"
-          />
-      </Container>
+      <>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Container fluid>
+                <Header />
+                <Navbar bg="success">
+                  <Form inline>
+                    <Form.Group >
+                      <Form.Control type="text" placeholder="enter a category of tool">
+                      </Form.Control>
+                      <Button className="ml-2">Submit</Button>
+                    </Form.Group>
+                  </Form>
+                </Navbar>
+                  <Card.Img
+                  alt='kitten' 
+                  src='http://placekitten.com/1920/600'
+                  className="radius-0"
+                  />
+              </Container>
+            </Route>
+          </Switch>
+        </Router>
+      </>
     )
   }
 }
--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,15 +1,16 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import {link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-function Footer() {
+function Header() {
     return (
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand>Tool Time</Navbar.Brand>
+        <Link to="/" className="mr-2">home</Link>
         <Button variant="outline-success" className="mr-2">Login</Button>
         <Button variant="outline-success">Logout</Button>
       </Navbar>
     )
 }
 
-export default Footer;
+export default Header;
