ca4aa1daa1e261ce7fe079d096814660391670c9
Dion Johnson <dionjwa@gmail.com>
2021-05-04 11:33:50 -0700

    Dions update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
 
+
 ReactDOM.render(
   <Auth0Provider
-    domain = {process.env.REACT_APP_DOMAIN}
-    clientId = {process.env.REACT_APP_CLIENT_ID}
-    redirectUri = {process.env.REACT_APP_REDIRECT_URI}
+    domain = 'dion.us.auth0.com'
+    clientId ='XPhvFe4CaurS6dWPgcFSNdeoDBHrND40'
+    redirectUri = 'http://localhost:3000'
   >
     <React.StrictMode>
      <App />
--- src/modules/LoginButton.js
+++ src/modules/LoginButton.js
@@ -4,14 +4,25 @@ import Button from 'react-bootstrap/Button';
 /* import '../Login.css'; */
 import { useAuth0 } from '@auth0/auth0-react';
 
-function Login() {
+// function Login() {
    
-    const { loginWithRedirect } = useAuth0();
-    return <Button 
-    onClick={() => loginWithRedirect()}
-    className='float-left'
-    >Log In</Button>;
+//     const { loginWithRedirect } = useAuth0();
+//     return <Button 
+//     onClick={() => loginWithRedirect()}
+//     className='float-left'
+//     >Log In</Button>;
  
-}
+// }
+function Login() {
+    const {
+      isAuthenticated,
+      loginWithPopup,
+    } = useAuth0();
+  
+    return !isAuthenticated && (
+      <Button onClick={loginWithPopup}>Log in</Button>
+    );
+  }
+  
 
 export default Login;
