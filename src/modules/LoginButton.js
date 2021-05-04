fd63295f35ebeaaa64742139917ecd766cb8100a
CullenSharp2 <cullen.sharp100@gmail.com>
2021-05-04 13:47:01 -0700

    Refactor and implement login with popup

--- src/modules/LoginButton.js
+++ src/modules/LoginButton.js
@@ -1,17 +1,15 @@
 import React from 'react';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-/* import '../Login.css'; */
 import { useAuth0 } from '@auth0/auth0-react';
 
 function Login() {
-   
-    const { loginWithRedirect } = useAuth0();
-    return <Button 
-    onClick={() => loginWithRedirect()}
-    className='float-left'
-    >Log In</Button>;
- 
+    const { loginWithPopup } = useAuth0();
+    return (
+      <Button 
+      onClick={() => loginWithPopup()}
+      className='float-left'
+      >Log In</Button>
+    )
 }
 
 export default Login;
