b705271b862f04a550c06cff5c1064a3afc57e44
CullenSharp2 <cullen.sharp100@gmail.com>
2021-05-04 10:28:05 -0700

    Remove auth0

--- src/modules/Profile.js
+++ src/modules/Profile.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { withAuth0 } from '@auth0/auth0-react';
-import { Container, Card } from 'react-bootstrap';
+// import { withAuth0 } from '@auth0/auth0-react';
+// import { Container, Card } from 'react-bootstrap';
 
 class Profile extends React.Component {
   render() {
-    const { user } = this.props.auth0;
+    // const { user } = this.props.auth0;
     return(
-      <Container>
-        <Card>
-          <h1>{`Welcome home, ${user.name}`}</h1>
-          <h2>{user.email}</h2>
-        </Card>
-        <Card>
-          <Card.Img src={user.picture}/>
-        </Card>
-      </Container>
+      <>
+        Something to come
+      </>
     );
   }
 }
 
-export default withAuth0(Profile);
+export default Profile;
