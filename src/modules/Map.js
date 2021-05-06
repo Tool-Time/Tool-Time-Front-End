6d0477b478d67b4bb0315f38491ad9df76609049
CullenSharp2 <cullen.sharp100@gmail.com>
2021-05-06 16:41:39 -0700

    General bugfix

--- src/modules/Map.js
+++ src/modules/Map.js
@@ -1,31 +1,22 @@
 import React from 'react';
-// import {Marker, InfoWindow} from 'react-bootstrap';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import toolshed from '../assets/toolShed.png'
 class MapContainer extends React.Component {
 
-  render() {
-    // set logic for user arrays to tie into auth0 user prop. 
-    
-const email = this.props.authUser.email;
-const currentUser = this.props.users.filter(user => user.email === email);
-
-
+  render() {  
+    const email = this.props.authUser.email;
+    const currentUser = this.props.allUsers.find(user => user.email === email);
     return (
       <>
-        <Map google={this.props.google} zoom={10} initialCenter={{ lat: currentUser[0].location[0], lng: currentUser[0].location[1] }}>
-
-         
+        <Map google={this.props.google} zoom={17} initialCenter={{ lat: currentUser.location[0], lng: currentUser.location[1] }}>
           <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{'Dope Place Coming Soonz'}</h1>
-            </div>
-
           </InfoWindow>
           {this.props.users.map((user) => (
             <Marker 
             position={{lat: user.location[0], lng: user.location[1]}}
             value={user._id} 
-            onClick={this.props.getSelectedUser} />
+            onClick={this.props.getSelectedUser} 
+            icon={toolshed}/>
               ))}
         </Map>
       </>
