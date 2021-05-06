8679109488bb23366a896cd5cbd462df83025017
Dion Johnson <dionjwa@gmail.com>
2021-05-05 21:53:36 -0700

    updated profile js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
-import { Container, Navbar} from 'react-bootstrap';
+import { Container, Navbar, } from 'react-bootstrap';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,34 +9,41 @@ import {
 } from 'react-router-dom';
 import Header from './modules/Header';
 import MapContainer from './modules/Map.js';
-import {withAuth0} from '@auth0/auth0-react';
+import { withAuth0 } from '@auth0/auth0-react';
+import Profile from './modules/Profile.js'
 
 const API_Server = process.env.REACT_APP_API_URL;
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       users: [],
-      selectedUser:[]
+      selectedUser: [],
+      showModal: false
     }
   }
 
-getSelectedUser= async (e) => {
-  const response = await axios.get(`${API_Server}user/${e.value}`);
-  this.setState({selectedUser: response.data})
-}
+  handleClose = () => {
+    this.setState({ showModal: false });
+  }
+
+  getSelectedUser = async (e) => {
+    const response = await axios.get(`${API_Server}user/${e.value}`);
+    this.setState({ selectedUser: response.data, showModal: true });
+    console.log(this.state.selectedUser);
+  }
 
   getUsers = async () => {
     const response = await axios.get(`${API_Server}`);
     const users = response.data;
     this.setState({ users });
   }
- 
+
   getQuery = async (e) => {
     e.preventDefault();
     const query = e.target.value.toLowerCase();
     const response = await axios.get(`${API_Server}users/?category=${query}`);
-    this.setState({users: response.data});
+    this.setState({ users: response.data });
   }
 
   async componentDidMount() {
@@ -44,24 +51,28 @@ getSelectedUser= async (e) => {
   }
 
   render() {
-    console.log(this.props.auth0);
-    return(
+   
+    return (
       <>
         <Router>
           <Switch>
             <Route path="/">
               <Container fluid>
-                <Header onChange={this.getQuery}/>
+                <Header onChange={this.getQuery} />
                 <Navbar bg="success">
                 </Navbar>
-                  {this.state.users.length && this.props.auth0.isAuthenticated &&
-                    <MapContainer 
-                    users={this.state.users} 
-                    getSelectedUser={this.getSelectedUser} 
+                {this.state.users.length && this.props.auth0.isAuthenticated &&
+                  <MapContainer
+                    users={this.state.users}
+                    getSelectedUser={this.getSelectedUser}
                     authUser={this.props.auth0.user}>
-                    </MapContainer>
-                  }
-                  {/* {this.state.selectedUser.length && <getUser> </getUser>} */}
+                  </MapContainer>
+                }
+                {this.state.showModal ?
+                  <Profile
+                    selectedUser={this.state.selectedUser} showModal={this.state.showModal} handleClose={this.handleClose}>
+                  </Profile> : ''
+                }
               </Container>
             </Route>
           </Switch>
--- src/modules/Profile.js
+++ src/modules/Profile.js
@@ -1,19 +1,32 @@
 import React from 'react';
 // import { withAuth0 } from '@auth0/auth0-react';
-// import { Container, Card } from 'react-bootstrap';
-
+import { Modal, Button } from 'react-bootstrap';
 class Profile extends React.Component {
+ 
   render() {
-    // const { user } = this.props.auth0;
-    return(
-      <Container>
-        <Card>
-          <h1>{`Welcome, ${user.name}`}</h1>
-          <h2>{user.email}</h2>
-        </Card>
-      </Container>
+ 
+    return (
+
+      <Modal show={this.props.showModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>{this.props.selectedUser.name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{this.props.selectedUser.tools[2].name}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={this.props.handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={this.props.handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+
+     
     );
-  }
+  };
 }
+
+
 
 export default Profile;
