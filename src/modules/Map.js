01503996cfcbdb5b9e6e4c4e5f3e01c743f3a7e5
CullenSharp2 <cullen.sharp100@gmail.com>
2021-05-06 19:08:56 -0700

    Remove comments

--- src/modules/Map.js
+++ src/modules/Map.js
@@ -3,7 +3,7 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import toolshed from '../assets/toolShed.png'
 class MapContainer extends React.Component {
 
-  render() {  
+  render() { 
     const email = this.props.authUser.email;
     const currentUser = this.props.allUsers.find(user => user.email === email);
     return (
@@ -23,9 +23,6 @@ class MapContainer extends React.Component {
     );
   }
 }
-
-//FISH TANK
-//  this.props.authUser.location[0], lon: this.props.authUser.location[1]}}>
 
 export default GoogleApiWrapper({
           apiKey: 'AIzaSyBSuLQnX_nzzV4VNBUBhqKB5yAKYIsTb3I'
